457ded9f06788f7c3fe927c987c4adcd6a7a4fff
MrKrrot <rafita.olguin@hotmail.com>
2023-02-05 22:03:54 -0600

    Added connected, disconected and typing features

--- src/index.js
+++ src/index.js
@@ -4,14 +4,35 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
+const players = []
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/pages/index.html')
 })
 
 io.on('connection', socket => {
+  let userId
+
   socket.on('chat message', msg => {
+    io.emit('typing', null)
     io.emit('chat message', msg)
+  })
+
+  socket.on('user', user => {
+    players.push(user)
+    userId = user
+    console.log(`${user} connected`)
+    io.emit('user', user)
+  })
+
+  socket.on('typing', (user) => {
+    socket.broadcast.emit('typing', user)
+  })
+
+  socket.on('disconnect', () => {
+    players.splice(players.indexOf(userId), 1)
+    io.emit('user left', userId)
+    console.log(`${userId} disconnected`)
   })
 })
 
