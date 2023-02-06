a65c7558ef9379d78b88c7f271995bd9b5418d09
MrKrrot <rafita.olguin@hotmail.com>
2023-02-05 20:56:41 -0600

    Ident spaces

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html')
+  res.sendFile(__dirname + '/pages/index.html')
 })
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg)
-    });
-  });
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+})
 
 server.listen(3000, () => {
-    console.log('Server listening on port 3000')
+  console.log('Server listening on port 3000')
 })
